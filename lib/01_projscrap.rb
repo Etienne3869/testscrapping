0e51d41cae153e6915318c1ed8a5f7176eb98aae
Etienne3869 <ekruajitch@gmail.com>
2021-04-22 21:24:29 +0200

    Dernier essaie

--- lib/01_projscrap.rb
+++ lib/01_projscrap.rb
@@ -1,6 +1,34 @@
-require 'rubygems'
-require 'nokogiri' 
+require 'nokogiri'
 require 'open-uri'
 
-page = Nokogiri::HTML(open("http://en.wikipedia.org/"))   
-puts page.class   # => Nokogiri::HTML::Document
+page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))  
+
+crypto_name = page.xpath('//tr/td[2]//a[contains(@href, "currencies")]')
+crypto_price = page.xpath('//tr/td[5]//a[contains(@href, "markets")]')
+
+crypto_name_text = []
+crypto_price_text = []
+
+crypto_name.each do |crypto_name|
+    crypto_name_text << crypto_name.text 
+  end
+
+crypto_price.each do |crypto_price|
+  crypto_price_text << crypto_price.text 
+end
+
+global = Hash[crypto_name_text.zip(crypto_price_text)]
+puts global
+
+
+
+#page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+
+
+#crypto_name = page.xpath('//a[@class="cmc-link"][@title]')
+#crypto_price = page.xpath('//tr/td[5]/a[contains(@href, "markets")]')
+
+
+#puts crypto_name
+#puts crypto_price
+
---
+++ spec/01_projscrap_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/dark_trader.rb'
+
+describe "Gives us the currencies names and their prices" do
+    it "Gives us the currencies names and their prices" do
+      expect(hello).to eq("Hello world!")
+    end
+  end
